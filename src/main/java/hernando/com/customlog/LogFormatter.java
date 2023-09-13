b24c155912466bb49bec9cc00aa645c7a53cbb77
Hernando Garcia Morales <hernandogm73@gmail.com>
2023-09-13 22:31:07 +0200

    update formatter class

--- src/main/java/hernando/com/customlog/LogFormatter.java
+++ src/main/java/hernando/com/customlog/LogFormatter.java
@@ -8,34 +8,34 @@ import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 public class LogFormatter extends Formatter {
-	private static final String OPEN_BRACKETS = "[";
-	private static final String CLOSE_BRACKETS = "] ";
 	private static final String SPACE = " ";
 	private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
 	/**
-     * Formats a log record into a string with a timestamp, log level, and message.
-     *
-     * @param record The log record to format.
-     * @return A formatted log message as a string.
-     */
-    @Override
+	 * Formats a log record into a string with a timestamp, log level, and message.
+	 *
+	 * @param record The log record to format.
+	 * @return A formatted log message as a string.
+	 */
+	@Override
 	public String format(LogRecord record) {
 		StringBuilder builder = new StringBuilder();
 		Instant instant = Instant.ofEpochMilli(record.getMillis());
 		ZonedDateTime zonedDateTime = instant.atZone(ZoneId.systemDefault());
-		builder.append(OPEN_BRACKETS).append(DATE_FORMATTER.format(zonedDateTime)).append(CLOSE_BRACKETS)
-				.append(OPEN_BRACKETS).append(record.getLevel()).append(CLOSE_BRACKETS);
+		builder.append(Constants.OPEN_BRACKETS).append(DATE_FORMATTER.format(zonedDateTime))
+				.append(Constants.CLOSE_BRACKETS).append(Constants.OPEN_BRACKETS).append(record.getLevel())
+				.append(Constants.CLOSE_BRACKETS);
+
 		setLogParameters(builder, record);
-		return builder.append("\n").toString();
+		return builder.append(Constants.ENTER).toString();
 	}
-    
+
 	/**
-     * Appends log parameters (variables) to the log message if present.
-     *
-     * @param builder The StringBuilder to which the log message is appended.
-     * @param record  The log record containing the message and parameters.
-     */
+	 * Appends log parameters (variables) to the log message if present.
+	 *
+	 * @param builder The StringBuilder to which the log message is appended.
+	 * @param record  The log record containing the message and parameters.
+	 */
 	private void setLogParameters(StringBuilder builder, LogRecord record) {
 		// Check if there are parameters (variables) to include
 		Object[] parameters = record.getParameters();
